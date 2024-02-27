6bb8d2564b7b51afe03b0c662c6d2e6a74ca756f
Phan Xuân Thái <22dh113377@st.huflit.edu.vn>
2024-02-27 19:50:14 +0700

    Sui

--- app/src/main/java/com/sinhvien/anhemtoicodedienthoai/LoginActivity.java
+++ app/src/main/java/com/sinhvien/anhemtoicodedienthoai/LoginActivity.java
@@ -2,11 +2,14 @@ package com.sinhvien.anhemtoicodedienthoai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+Button login, register, forgotpass , asguest ;
 
 
 
@@ -15,7 +18,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addControl();
+        asguest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeFragment.class);
+            }
+        });
     }
 
+
+    public void addControl(){
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+        forgotpass=findViewById(R.id.forgotpass);
+        asguest=findViewById(R.id.asguest);
+    }
 }
