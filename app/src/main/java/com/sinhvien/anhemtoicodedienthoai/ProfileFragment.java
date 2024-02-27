cfc84da14ff5fb4e0c810a5f5035afe1b65fcc17
Phan Xuân Thái <22dh113377@st.huflit.edu.vn>
2024-02-27 19:22:16 +0700

    .

--- app/src/main/java/com/sinhvien/anhemtoicodedienthoai/ProfileFragment.java
+++ app/src/main/java/com/sinhvien/anhemtoicodedienthoai/ProfileFragment.java
@@ -1,12 +1,15 @@
 package com.sinhvien.anhemtoicodedienthoai;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+Button btnlq;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -44,21 +48,41 @@ public class ProfileFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+
+
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+       btnlq=view.findViewById(R.id.btnlg);
+        btnlq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    return view;
+
+
     }
 }
